533ab5141523abc33863b7306fc24511e0595d39
Jacob Gomez Carrillo <husky-coldjacob@hotmail.com>
2026-01-15 13:47:15 -0600

    fixed url image header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,14 +7,14 @@ function Header({carrito, removeFromCart, increaseQuantity, decreaseQuantity, cl
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src="./img/logo.svg" alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div 
                             className="carrito"
                         >
-                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+                            <img className="img-fluid" src="./img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
                                 
@@ -38,7 +38,7 @@ function Header({carrito, removeFromCart, increaseQuantity, decreaseQuantity, cl
 
                                                     <tr key={guitar.id}>
                                                         <td>
-                                                            <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+                                                            <img className="img-fluid" src={`./img/${guitar.image}.jpg`} alt="imagen guitarra" />
                                                         </td>
                                                         <td>{guitar.name}</td>
                                                         <td className="fw-bold">
